7950428bce2a97702c87d874241f2e22f73d8458
Jhon Torres <Jhon.Torres@ilumno.com>
2024-07-18 10:46:53 -0500

    desarrollo final

--- index.js
+++ index.js
@@ -12,15 +12,80 @@ const PRIVATE_APP_ACCESS = '';
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
-// * Code for Route 1 goes here
+app.get('/', async (req, res) => {
+    const hijo = 'https://api.hubspot.com/crm/v3/objects/2-32314521?properties=nombre,edad,sexo,identificacion';
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    }
+    try {
+        const resp = await axios.get(hijo, { headers });
+        const data = resp.data.results;
+        res.render('hijo', { title: 'hijo | HubSpot APIs', data });
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
-// * Code for Route 2 goes here
+app.get('/create', async (req, res) => {
+    try {
+        res.render('create');
+    } catch (error) {
+        console.error(error);
+    }
+});
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
+app.post('/create', async (req, res) => {
+    const create = {
+        properties: {
+            "nombre": req.body.nombre,
+            "edad": req.body.edad,
+            "identificacion": req.body.identificacion,
+            "sexo": req.body.sexo
+        }
+    }
 
-// * Code for Route 3 goes here
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    };
+
+    const identificacion = req.body.identificacion;
+    const searchhijo = `https://api.hubspot.com/crm/v3/objects/2-32314521/${identificacion}?idProperty=identificacion`
+    let updateId;
+    try {
+        const resp = await axios.get(searchhijo, { headers });
+        const data = resp.data;
+        updateId = data.id;
+    } catch (error) {
+        console.error(error);
+    }
+
+    if (!updateId) {
+        const createhijo = `https://api.hubapi.com/crm/v3/objects/2-32314521`;
+        try {
+            const response = await axios.post(createhijo, create, { headers });
+            res.redirect('/');
+        } catch (err) {
+            res.redirect('/');
+            console.error(err);
+        }
+    } else {
+        const updatehijo = `https://api.hubapi.com/crm/v3/objects/2-32314521/${updateId}`;
+        try {
+            const response = await axios.patch(updatehijo, create, { headers });
+            res.redirect('/');
+        } catch (err) {
+            res.redirect('/');
+            console.error(err);
+        }
+    }
+});
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
